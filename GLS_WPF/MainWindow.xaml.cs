fc3926623b7635c401221224dac9ed8df9f92c2b
qgedeak@gmail.com <qgedeak@gmail.com>
2026-02-25 09:10:00 +0100

    WPF 1 feladat

---
+++ GLS_WPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GLS_CLI1;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace GLS_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] adatok = File.ReadAllLines("GLS.txt");
+            List<AutoAdatok> autoLista = new List<AutoAdatok>();
+            foreach (var item in adatok)
+            {
+                autoLista.Add(new AutoAdatok(item));
+            }
+            datagrid.ItemsSource = autoLista;
+        }
+    }
+}
