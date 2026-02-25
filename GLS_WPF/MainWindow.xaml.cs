f6ee89b7fc124a0bf62ebb1216edf53f913260a8
qgedeak@gmail.com <qgedeak@gmail.com>
2026-02-25 09:58:21 +0100

    módosítás - Kósa Áron

--- GLS_WPF/MainWindow.xaml.cs
+++ GLS_WPF/MainWindow.xaml.cs
@@ -33,6 +33,10 @@ namespace GLS_WPF
 
         private void Felvitel_Click(object sender, RoutedEventArgs e)
         {
+            if (validalas() == false)
+            {
+                return;
+            }
             if (autoLista.Find(x=>x.datum == datumBox.Text) != null)
             {
                 MessageBox.Show("Már van ilyen dátum", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
@@ -45,7 +49,46 @@ namespace GLS_WPF
 
         private void Modositas_Click(object sender, RoutedEventArgs e)
         {
+            if (validalas() == false)
+            {
+                return;
+            }
+            autoLista.Remove(datagrid.SelectedItem as AutoAdatok);
+            if (autoLista.Find(x => x.datum == datumBox.Text) != null)
+            {
+                MessageBox.Show("Már van ilyen dátum", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            AutoAdatok adat = new AutoAdatok(datumBox.Text, nevBox.Text, int.Parse(csomagokBox.Text), int.Parse(fogyasztasBox.Text), int.Parse(kmBox.Text));
+            autoLista.Add(adat);
+            datagrid.Items.Refresh();
 
+        }
+        private bool validalas()
+        {
+            if(datumBox.Text==""||nevBox.Text==""||csomagokBox.Text==""||fogyasztasBox.Text=="") 
+            {
+                MessageBox.Show("Hibás vagy hiányzó adat!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false; 
+            }
+            return true;
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (datagrid.SelectedItem != null)
+            {
+                if (datagrid.SelectedItem is AutoAdatok)
+                {
+                    datumBox.Text = ((AutoAdatok)datagrid.SelectedItem).datum;
+                    nevBox.Text = ((AutoAdatok)datagrid.SelectedItem).sofNev;
+                    csomagokBox.Text = ((AutoAdatok)datagrid.SelectedItem).kezbCsomagSzam.ToString();
+                    fogyasztasBox.Text = ((AutoAdatok)datagrid.SelectedItem).napiFogyasztasLiter.ToString();
+                    kmBox.Text = ((AutoAdatok)datagrid.SelectedItem).napiKilometer.ToString();
+
+                }
+            }
+            
         }
     }
 }
