2e6a2387955ae2e037ff3edd4261ab6a37845f10
qgedeak@gmail.com <qgedeak@gmail.com>
2026-02-25 08:11:17 +0100

    autotvezeto soforok szama

--- GLS_CLI1/Program.cs
+++ GLS_CLI1/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] adatok = File.ReadAllLines("GLS.txt");
+            List<AutoAdatok> autoLista = new List<AutoAdatok>();
+            foreach (var item in adatok)
+            {
+                autoLista.Add(new AutoAdatok(item));
+            }
+
+            Console.WriteLine($"Az autó használatban töltött napjainak száma: {autoLista.Count()}");
+
+            HashSet<string> soforok = new HashSet<string>();
+            foreach (var item in autoLista)
+            {
+                soforok.Add(item.sofNev);
+            }
+            Console.WriteLine($"Különböző sofőrok száma: {soforok.Count()}");
         }
     }
 }
